cdda70aca8605af626ece35e5d456488d905b1d5
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-06 15:43:42 -0500

    contact modal complete

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const showAbout = (e) => {
         viewButton = document.querySelector('#view'),
         hideButton = document.querySelector('#back'),
         caseStudies = document.querySelector('#case')
-    resume = document.querySelector('#resume'),
+        resume = document.querySelector('#resume'),
         clickLocation = e.screenY;
 
     aboutContainer.classList.remove('more', 'about--before');
@@ -33,8 +33,13 @@ const show = document.querySelector('#about').addEventListener('click', showAbou
 // Displays modal when user clicks 'click' on hero image
 const showModal = () => {
     let modal = document.querySelector('#modal'),
+        contact = document.querySelector('#contact-form'),
+        closeContact = document.querySelector('#close-contact'),
         close = document.querySelector('#close');
+
+    closeContact.classList.add('hide');
     modal.classList.remove('hide');
+    contact.classList.add('hide')
 }
 
 // Event listener for showing modal after clicking 'click'
@@ -48,21 +53,44 @@ const expandModal = () => {
         buttonTwo = document.querySelector('#notInterested'),
         modalTitle = document.querySelector('#modal-title'),
         modalBody = document.querySelector('#modal-body'),
+        closeContact = document.querySelector('#close-contact'),
         close = document.querySelector('#close');
 
-    close.classList.add('hide');
     buttonOne.classList.add('hide');
     buttonTwo.classList.add('hide');
     modalTitle.classList.add('hide');
     modalBody.classList.add('hide');
+    close.classList.add('hide');
+    closeContact.remove('hide');
+
     container.classList.add('content--after');
     container.classList.remove('content');
     modal.classList.remove('modal');
     modal.classList.add('modal--after');
+    
+    setTimeout(() => {
+        showContact();
+    }, 300);
 }
 
 // Event listener for showing contact form
 const expand = document.querySelector('#interested').addEventListener('click', expandModal);
+
+const showContact = () => {
+    let contact = document.querySelector('#contact-form'),
+        form = document.querySelector('#form'),
+        contactTitle = document.querySelector('#contact-title'),
+        backgroundContent = document.querySelector('#content'),
+        contactBody = document.querySelector('#contact-body');
+
+    contact.classList.remove('hide');
+    form.classList.remove('hide');
+    contactTitle.classList.remove('hide');
+    contactBody.classList.remove('hide');
+    console.log('success')
+}
+
+const contactNav = document.querySelector('#navContact').addEventListener('click', showContact)
 
 const hideModal = () => {
     let modal = document.querySelector('#modal'),
