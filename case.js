45ff4e6e1d3739c1a19ffeada70c742df03f6911
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-07 16:46:22 -0500

    lightbox feature is kind of working

--- case.js
+++ case.js
@@ -1,31 +1,64 @@
 // Scrolls to wavefire case study
 const scrollToWave = () => {
-    const wavefire = document.querySelector('#wavefireStudy'),          location = wavefire.getBoundingClientRect();
+    const wavefire = document.querySelector('#wavefireStudy'), location = wavefire.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
 // Scrolls to splitsecnd case study
 const scrollToSplit = () => {
-    const splitsecnd = document.querySelector('#splitsecndStudy'),          location = splitsecnd.getBoundingClientRect();
+    const splitsecnd = document.querySelector('#splitsecndStudy'), location = splitsecnd.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
 // Scrolls to edgenet section
 const scrollToEdge = () => {
-    const edgenet = document.querySelector('#edgenetStudy'),          location = edgenet.getBoundingClientRect();
+    const edgenet = document.querySelector('#edgenetStudy'), location = edgenet.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
 // Scrolls to asp section
 const scrollToASP = () => {
-    const asp = document.querySelector('#aspStudy'),          location = asp.getBoundingClientRect();
+    const asp = document.querySelector('#aspStudy'), location = asp.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
 const wavefireClick = document.querySelector('#wavefire').addEventListener('click', scrollToWave);
-
 const aspClick = document.querySelector('#asp').addEventListener('click', scrollToASP);
-
 const splitsecndClick = document.querySelector('#splitsecnd').addEventListener('click', scrollToSplit);
-
 const edgenetClick = document.querySelector('#edgenet').addEventListener('click', scrollToEdge);
+
+
+const lightBox = (e) => {
+    let imageId = e.target.id,
+        lightboxModal = document.querySelector('#lightbox'),
+        lightboxContent = document.querySelector('#content'),
+        clickedImage,
+        preview;
+
+    if (lightboxContent.childNodes.length == 1) {
+        if (e.srcElement.nodeName === 'IMG') {
+            imageURL = e.target.src;
+            lightboxModal.classList.remove('hide');
+
+            preview = document.createElement('img');
+            preview.src = imageURL;
+            preview.classList.add('modal--image')
+
+            lightboxContent.appendChild(preview);
+        }
+    }
+}
+
+const lightbox = document.querySelector('#caseStudies').addEventListener('click', lightBox)
+
+
+const hideModal = () => {
+    lightboxModal = document.querySelector('#lightbox'),
+    lightboxContent = document.querySelector('#content'),
+    close = document.querySelector('#closeLight'); 
+    lightboxModal.classList.add('hide');
+}
+
+// Event listeners for closing modal
+const closeLight = document.querySelector('#closeLight').addEventListener('click', hideModal);
+
