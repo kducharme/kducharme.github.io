ad336dc3fa7e4f96516c8736d2ce1c80f90a1023
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-07 10:31:16 -0500

    sticky sidebar working

--- case.js
+++ case.js
@@ -4,20 +4,20 @@ const scrollToWave = () => {
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
-const wavefireClick = document.querySelector('#wavefire').addEventListener('click', scrollToWave);
-
 // Scrolls to splitsecnd case study
 const scrollToSplit = () => {
     const splitsecnd = document.querySelector('#splitsecndStudy'),          location = splitsecnd.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
 
-const splitsecndClick = document.querySelector('#splitsecnd').addEventListener('click', scrollToSplit);
-
 // Scrolls to edgenet section
 const scrollToEdge = () => {
     const edgenet = document.querySelector('#edgenetStudy'),          location = edgenet.getBoundingClientRect();
     window.scroll({ top: location.top - 30, left: 0, behavior: 'smooth' });
 }
+
+const wavefireClick = document.querySelector('#wavefire').addEventListener('click', scrollToWave);
+
+const splitsecndClick = document.querySelector('#splitsecnd').addEventListener('click', scrollToSplit);
 
 const edgenetClick = document.querySelector('#edgenet').addEventListener('click', scrollToEdge);
