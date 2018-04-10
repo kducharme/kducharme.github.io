ce7dc09f1af5421f5a43509f1f253687c6dce34c
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-09 20:41:24 -0500

    completed responsive nav menu

--- case.js
+++ case.js
@@ -1,4 +1,4 @@
-// Scrolls to wavefire case study
+// Jumps to section after user clicks side nav
 const jumpTo = (e) => {
     const sectionClicked = e.target.id;
     let location = 0;
@@ -11,13 +11,14 @@ const jumpTo = (e) => {
     }
 
     location = document.querySelector(jobs[e.target.id]).getBoundingClientRect();
-    
+
     window.scroll({ top: location.top, left: 0, behavior: 'smooth' });
 }
 
 const jumpToLink = document.querySelector('#side-nav').addEventListener('click', jumpTo)
 
 
+// Lightbox feature for clicking on images to view larger
 const lightBox = (e) => {
     let imageId = e.target.id,
         lightboxModal = document.querySelector('#lightbox'),
@@ -42,29 +43,37 @@ const lightBox = (e) => {
 
 const lightbox = document.querySelector('#caseStudies').addEventListener('click', lightBox)
 
-
+// Clicking outside of modal or 'x' closes it out
 const hideModal = () => {
-    lightboxModal = document.querySelector('#lightbox'),
-    lightboxContent = document.querySelector('#content'),
-    close = document.querySelector('#closeLight'); 
-    lightboxModal.classList.add('hide');
+    const lightboxModal = document.querySelector('#lightbox');
+    const lightboxContent = document.querySelector('#content');
+    const close = document.querySelector('#closeLight');
 
+    lightboxModal.classList.add('hide');
     lightboxContent.removeChild(lightboxContent.childNodes[0, 1])
 }
 
 // Event listeners for closing modal
 const closeLight = document.querySelector('#closeLight').addEventListener('click', hideModal);
-// const closeLightBox = document.querySelector('#lightboxb').addEventListener('click', hideModal);
 
-const responsiveMenu = () => {
-    let navStructure = document.querySelector('nav'),
-        navLinks = document.querySelector('#primary-nav');
-    
-    navLinks.classList.remove('nav__links');
 
-    console.log('success')
+const showMenu = () => {
+    const primaryNav = document.querySelector('#primary-nav');
+    const responsiveNav = document.querySelector('#responsive-nav');
 
-    }
+    primaryNav.classList.add('hide');
+    responsiveNav.classList.remove('hide');
+}
 
-const hamburger = document.querySelector('#menu').addEventListener('click', responsiveMenu);
+const hamburger = document.querySelector('#menu').addEventListener('click', showMenu);
+
+const hideMenu = () => {
+    const primaryNav = document.querySelector('#primary-nav');
+    const responsiveNav = document.querySelector('#responsive-nav');
+
+    primaryNav.classList.remove('hide');
+    responsiveNav.classList.add('hide');
+}
+
+const exit = document.querySelector('#close-menu').addEventListener('click', hideMenu);
 
