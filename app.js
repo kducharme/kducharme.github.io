e7e20c8d1d7dde83c7508253f5b1bc8ca0829535
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-14 10:41:14 -0500

    fixed homepage responsiveness

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ const hideContact = () => {
 }
 
 const closeContact = document.querySelector('#closeContact').addEventListener('click', hideContact)
+
+const showMenu = () => {
+    const primaryNav = document.querySelector('#primary-nav');
+    const responsiveNav = document.querySelector('#responsive-nav');
+
+    primaryNav.classList.add('hide');
+    responsiveNav.classList.remove('hide');
+}
+
+const hamburger = document.querySelector('#menu').addEventListener('click', showMenu);
+
+const hideMenu = () => {
+    const primaryNav = document.querySelector('#primary-nav');
+    const responsiveNav = document.querySelector('#responsive-nav');
+
+    primaryNav.classList.remove('hide');
+    responsiveNav.classList.add('hide');
+}
+
+const exit = document.querySelector('#close-menu').addEventListener('click', hideMenu);
