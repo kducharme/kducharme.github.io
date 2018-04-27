f5e4d55c2b022100f87de0bfa036455948f80145
Kyle Ducharme <ducharme.kyle@gmail.com>
2018-04-26 20:28:48 -0500

    fixed js

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,3 @@
-const showContactForm = () => {
-    const modal = document.querySelector('#contact-modal');
-    const body = document.querySelector('body');
-    const nav = document.querySelector('nav');
-    
-    modal.classList.toggle('prevent-scroll')
-    modal.classList.toggle('hide');
-    // body.classList.toggle('prevent-scroll');
-}
-
-const responsiveContact = document.querySelector('#responsiveContact').addEventListener('click', showContactForm)
-
-const clickConstactInNav = document.querySelector('#navContact').addEventListener('click', showContactForm)
-
-const hideContact = () => {
-    const contactModal = document.querySelector('#contact-modal');
-    const modal = document.querySelector('#modal');
-    const body = document.querySelector('body');
-
-    contactModal.classList.add('hide');
-    modal.classList.add('hide');
-    body.classList.toggle('prevent-scroll');
-}
-
-
 const closeContact = document.querySelector('#closeContact').addEventListener('click', hideContact)
 
 const showMenu = () => {
